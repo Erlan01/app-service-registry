85a6b399f14cd0c62e88cdeb8977da19247f29fc
Erlan01 <87492684+Erlan01@users.noreply.github.com>
2022-08-14 22:59:55 +0500

    Completed

---
+++ src/main/java/ai/ecma/appserviceregistry/AppServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package ai.ecma.appserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AppServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServiceRegistryApplication.class, args);
+    }
+
+}
